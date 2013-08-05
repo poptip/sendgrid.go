3f493fffe628448c4c1630487e9c44d8faf0fcae
Roly Fentanes <roly426@gmail.com>
2013-08-05 15:22:11 -0400

    post request

--- sendgrid.go
+++ sendgrid.go
@@ -41,15 +41,31 @@ func NewClient(username, password string) *Client {
 
 // getBase returns the base url that will be used for the API request.
 func (c Client) getBase(command string) string {
-	return Host + "/api/" + command + ".json?api_user=" +
-		url.QueryEscape(c.username) + "&api_key=" + c.password
+	return Host + "/api/" + command + ".json?"
 }
 
-func (c Client) get(u uri) *SendGridError {
-	resp, err := http.Get(string(u))
+func (c Client) setCredentials(v *url.Values) {
+	v.Set("api_user", c.username)
+	v.Set("api_key", c.password)
+}
+
+func (c Client) get(u string, v url.Values) *SendGridError {
+	resp, err := http.Get(u + v.Encode())
 	if err != nil {
 		return &SendGridError{Message: err.Error()}
 	}
+	return c.response(resp)
+}
+
+func (c Client) post(u string, v url.Values) *SendGridError {
+	resp, err := http.PostForm(u, v)
+	if err != nil {
+		return &SendGridError{Message: err.Error()}
+	}
+	return c.response(resp)
+}
+
+func (c Client) response(resp *http.Response) *SendGridError {
 	defer resp.Body.Close()
 
 	// Read the JSON message from the body.
@@ -62,7 +78,9 @@ func (c Client) get(u uri) *SendGridError {
 	}
 
 	// Check if the response is successful.
-	if len(response.Errors) > 0 || resp.StatusCode != 200 || response.Message != "success" {
+	if len(response.Errors) > 0 ||
+		resp.StatusCode != 200 ||
+		response.Message != "success" {
 		return &SendGridError{StatusCode: resp.StatusCode, Response: response}
 	}
 
@@ -72,25 +90,27 @@ func (c Client) get(u uri) *SendGridError {
 // MailSend will send out emal o list of given recipients.
 func (c Client) MailSend(args MailArgs) *SendGridError {
 	// Create the address.
-	u := uri(c.getBase("mail.send"))
-	u.appendArray("to", args.To)
-	u.appendArray("toname", args.ToName)
-	u.appendArray("bcc", args.Bcc)
-	u.appendValue("from", args.From)
-	u.appendValue("fromname", args.FromName)
-	u.appendValue("subject", args.Subject)
-	u.appendValue("text", args.Text)
-	u.appendValue("html", args.Html)
-	if err := u.appendMap("x-smtapi", args.Xsmtapi); err != nil {
+	base := c.getBase("mail.send")
+	v := url.Values{}
+	c.setCredentials(&v)
+	addArray(&v, "to", args.To)
+	addArray(&v, "toname", args.ToName)
+	addArray(&v, "bcc", args.Bcc)
+	v.Add("from", args.From)
+	v.Add("fromname", args.FromName)
+	v.Add("subject", args.Subject)
+	v.Add("text", args.Text)
+	v.Add("html", args.Html)
+	if err := addMap(&v, "x-smtapi", args.Xsmtapi); err != nil {
 		return &SendGridError{Message: err.Error()}
 	}
-	if err := u.appendMap("content", args.Content); err != nil {
+	if err := addMap(&v, "content", args.Content); err != nil {
 		return &SendGridError{Message: err.Error()}
 	}
-	if err := u.appendMap("headers", args.Headers); err != nil {
+	if err := addMap(&v, "headers", args.Headers); err != nil {
 		return &SendGridError{Message: err.Error()}
 	}
 
 	// Make the request.
-	return c.get(u)
+	return c.post(base, v)
 }
