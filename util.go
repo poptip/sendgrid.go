eaee6854dd47a835d01d5829246a7d995770073b
Roly Fentanes <roly426@gmail.com>
2013-08-05 15:21:52 -0400

    use url.Values

---
+++ util.go
@@ -0,0 +1,27 @@
+package sendgrid
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/url"
+)
+
+// appendArray adds a list in url format.
+func addArray(v *url.Values, name string, list []string) {
+	for _, value := range list {
+		v.Add(name, value)
+	}
+}
+
+// appendMap adds a key value pairing map to the uri
+func addMap(v *url.Values, name string, data map[string]string) error {
+	if len(data) == 0 {
+		return nil
+	}
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	v.Add(name, bytes.NewBuffer(b).String())
+	return nil
+}
